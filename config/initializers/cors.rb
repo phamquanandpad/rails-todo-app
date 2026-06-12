4dbd959127a8dba63f38cc510ca15fee69d02fad
phamquanandpad <quan.pham@andpad.co.jp>
2026-06-12 15:37:31 +0700

    feat: config cors for production

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173"
+    origins "http://localhost:5173",
+            "https://phamquanandpad.github.io"
 
     resource "/api/*",
       headers: :any,
